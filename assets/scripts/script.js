847b6999010c76635761abd0d9090eff90f36902
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-04-27 15:27:30 +0000

    Add comments to js file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,5 @@
-/* Toggle #cd div on Sessions & Fees Page */
+/* Toggle #cd div on Sessions & Fees Page 
+   Credit: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp */
 function myFunction() {
     var x = document.getElementById("cd");
     if (x.style.display === "none") {
@@ -8,7 +9,8 @@ function myFunction() {
     }
   }
 
-/* Responsive Navigation */
+/* Responsive Navigation 
+  Credit: https://dev.to/devggaurav/let-s-build-a-responsive-navbar-and-hamburger-menu-using-html-css-and-javascript-4gci */
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("nav ul");
 const contactLink = document.querySelector("#contact-link");
